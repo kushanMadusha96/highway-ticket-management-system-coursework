b723c441838ae84f85b75cc14408a582d1a78fb8
kushanMadusha96 <kushanmadusha96@gmail.com>
2024-06-26 19:04:25 +0530

    add configuration server

---
+++ configuration-server/src/test/java/lk/ijse/configuration_server/configuration/server/ConfigurationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.configuration_server.configuration.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigurationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
